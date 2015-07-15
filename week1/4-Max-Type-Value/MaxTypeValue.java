35e837ab00d854787a9e0c76fae8b48f9f05d598
elly2chan <elly2chan@abv.bg>
2015-07-15 19:37:52 +0300

    Create MaxTypeValue.java

---
+++ week1/4-Max-Type-Value/MaxTypeValue.java
@@ -0,0 +1,12 @@
+public class MaxTypeValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Integer Max Value: "+Integer.MAX_VALUE);
+		System.out.println("Long Max Value: " + Long.MAX_VALUE);
+		System.out.println("Float Max Value: " + Float.MAX_VALUE);
+		System.out.println("Double Max Value: " + Double.MAX_VALUE);
+		 
+	}
+
+}
