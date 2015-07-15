b0a086ffca47c2fcff9f565bd0cbc920b51ceacf
elly2chan <elly2chan@abv.bg>
2015-07-15 20:35:36 +0300

    Interface

---
+++ week6/2-Zombie-Apocalypse/Weapon.java
@@ -0,0 +1,11 @@
+public interface Weapon {
+	
+	public String getName();
+	
+	public int getDamage();
+
+	public int getDurability();
+
+	public String getType(); // can return "TO_SMASH" or "TO_SHOOT"
+
+}
