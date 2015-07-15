e707924b6fa40e2312c230bd5153427a7f2a2ede
elly2chan <elly2chan@abv.bg>
2015-07-15 20:06:25 +0300

    Create Song.java

---
+++ week4/1-Music-Organizer/Song.java
@@ -0,0 +1,21 @@
+public class Song {
+    private String name;
+    private String artist;
+    private int duration;
+    private String album;
+
+    public Song(String name, String artist, int duration, String album){
+        this.name = name;
+        this.artist = artist;
+        this.duration = duration;
+        this.album = album;
+    }
+
+    public String toString(){
+        return name + " " + artist + " " + duration + " " + album;
+    }
+
+    public String getAlbum(){
+        return album;
+    }
+}
