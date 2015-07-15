fa0e8d0d6566507aab88436c51b875e795b1a31a
elly2chan <elly2chan@abv.bg>
2015-07-15 20:31:28 +0300

    Create Main.java

---
+++ week6/1-Landscape/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	 public static void main(String[] args) {
+		    Landscape landscape = new Landscape();
+
+		    landscape.addTile(new Land());
+		    landscape.addTile(new Water());
+		    landscape.addTile(new Mountain());
+		    landscape.addTile(new Forest());
+
+		    System.out.println(landscape.renderAll()); // --~~/\!!
+		  }
+		}
