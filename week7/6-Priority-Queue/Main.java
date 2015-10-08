812c28ea1b6f0bf316eead01d0cf6b08c9c48b08
elly2chan <elly2chan@abv.bg>
2015-10-08 21:04:50 +0300

    Create Main.java

---
+++ week7/6-Priority-Queue/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        PriorityQueue queue = new PriorityQueue();
+        queue.push(60);
+        queue.push(75);
+        queue.push(85);
+        queue.push(50);
+        queue.push(56);
+
+        System.out.println(queue.pop()); // 85
+        System.out.println(queue.pop()); // 75
+        System.out.println(queue.pop()); // 60
+    }
+}
