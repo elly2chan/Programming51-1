0c4a13c7a9223b08daf11bc456fcd32975634c57
elly2chan <elly2chan@abv.bg>
2015-10-08 20:58:39 +0300

    Update StringReverse.java

--- week7/3-String-Reverse/StringReverse.java
+++ week7/3-String-Reverse/StringReverse.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StringUtils {
+public class StringReverse {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
