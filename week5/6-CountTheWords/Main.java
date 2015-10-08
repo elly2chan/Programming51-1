a526d9ec8d21ae105fce2d530fb604764264e143
elly2chan <elly2chan@abv.bg>
2015-10-08 20:46:48 +0300

    Create Main.java

---
+++ week5/6-CountTheWords/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String searched = scanner.next();
+        char [][] map;
+        int n,m;
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        map = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                map[i][j] = scanner.next().charAt(0);
+            }
+        }
+
+        WordCounter counter = new WordCounter(map, searched);
+
+        // Print result here
+        System.out.println(counter.findTheWord());
+    }
+}
